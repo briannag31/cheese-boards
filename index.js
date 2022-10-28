c6f188fb84cdd4fd1e136fe01bcd7a0f3dd90df2
Brianna Gaines <briannagaines31@gmail.com>
2022-10-27 17:09:27 -0700

    establishes Model associations

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ const {Board} = require('./Board')
 const {Cheese} = require('./Cheese')
 const {User} = require("./User")
 
+Board.belongsTo(User)
+User.hasMany(Board)
+
+Board.belongsToMany(Cheese, {through: "cheese_board"})
+Cheese.belongsToMany(Board, {through: "cheese_board"})
+
 
 module.exports = {
     Board,
